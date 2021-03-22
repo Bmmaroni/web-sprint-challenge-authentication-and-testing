016334e3f6d9176f8590fe264a6fe8f9bd3d10f0
Brandon Maroni <63432771+Bmmaroni@users.noreply.github.com>
2021-03-21 19:47:34 -0600

    finished sprint challenge MVP met

--- api/server.test.js
+++ api/server.test.js
@@ -6,9 +6,9 @@ test('sanity', () => {
   expect(true).not.toBe(false)
 })
 
-beforeEach(async () => {
-	await db.seed.run()
-})
+// beforeEach(async () => {
+// 	await db.seed.run()
+// })
 
 beforeAll(async () => {
 	await db.migrate.rollback()
@@ -24,14 +24,14 @@ describe("auth integration tests", () => {
 		const res = await supertest(server)
 			.post("/api/auth/register")
 			.send({
-				username: "Bill",
+				username: "John",
 				password: "abc123"
 			})
 		const users = await db("users")
 		expect(res.statusCode).toBe(201)
 		expect(res.headers["content-type"]).toBe("application/json; charset=utf-8")
-		expect(res.body.username).toBe("Bill")
-		expect(users).toHaveLength(2)
+		expect(res.body.username).toBe("John")
+		expect(users).toHaveLength(1)
 	})
 
 	it("won't duplicate a user", async () => {
@@ -42,9 +42,10 @@ describe("auth integration tests", () => {
 				password: "abc123"
 			})
 		expect(res.body.message).toBe("username taken")
+		expect(res.statusCode).toBe(401)
 	})
 
-	it.only("successfully logins user", async () => {
+	it("successfully logins user", async () => {
 		const res = await supertest(server)
 			.post("/api/auth/login")
 			.send({
@@ -52,5 +53,15 @@ describe("auth integration tests", () => {
 				password: "abc123"
 			})
 		expect(res.statusCode).toBe(200)
+	})
+
+	it("gives user a token on login", async () => {
+		const res = await supertest(server)
+			.post("/api/auth/login")
+			.send({
+				username: "John",
+				password: "abc123"
+			})
+		expect(res.cookie).toBe(res.headers.authorization)
 	})
 })
